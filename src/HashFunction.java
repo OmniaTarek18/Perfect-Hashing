2f6af690f32993a3ae46514462a36e35f4c3f52d
OmniaTarek18 <120803508+OmniaTarek18@users.noreply.github.com>
2024-04-19 00:38:15 +0200

    implement all hashfunction methods

--- src/HashFunction.java
+++ src/HashFunction.java
@@ -1,38 +1,108 @@
+import java.util.Random;
 
 public class HashFunction {
-	// random the matrix (until rehashing change the matrix)
 	private int row;
 	private int[][] matrix;
-	static int col = 64;
+	private final int col = 64;
 
-	static void RandomiseMatrix() {
-
-//			randomize the matrix here with 0/1
+	/*
+	 * our constructor takes the rows and randomize the matrix
+	 */
+	public HashFunction(int row) {
+		this.row = row;
+		this.matrix = new int[this.row][this.col];
+		this.RandomiseMatrix();
 	}
 
 	/*
-	 * our constructor takes the rows and random the matrix
+	 * randomize the matrix here with 0 or 1
 	 */
-	public HashFunction(int row) {
-			this.row = row;
-			RandomiseMatrix();
+	private void RandomiseMatrix() {
+		Random rand = new Random();
+		for (int i = 0; i < this.row; i++) {
+			for (int j = 0; j < this.col; j++) {
+				this.matrix[i][j] = rand.nextInt(2);
+			}
 		}
+	}
 
 	/*
 	 * Do the Multiplication and return the index
 	 */
-	public long getIndex() {
-		return 0;
+	private long getIndex(long key) {
+
+		int[] binaryKey = this.convertToBinary(key);
+		// multiplication
+		int[] index = new int[this.row];
+		for (int i = 0; i < this.row; i++) {
+			for (int j = 0; j < this.col; j++) {
+				index[i] += this.matrix[i][j] * binaryKey[j];
+			}
+			index[i] &= 1; // bitwise operation (index % 2 )
+		}
+
+		long decimalIndex = this.convertToDecimal(index);
+
+		return decimalIndex;
 	}
 
-//		convert key to binary
-	public static long convertToBinary(long key) {
-		return 0;
+	/*
+	 * convert key to binary -> 8 -> [1,0,0,0]
+	 */
+	private int[] convertToBinary(long key) {
+		int[] binaryKey = new int[64];
+		for (int i = 64 - 1; i >= 0; i--) {
+			binaryKey[i] = (int) (key & 1);
+			key >>= 1;
+		}
+		return binaryKey;
 	}
 
-//		convert index to decimal
-	public static long convertToDecimal() {
-		return 0;
+	/*
+	 * convert index to decimal using doubling method
+	 */
+	private long convertToDecimal(int[] index) {
+		long decimalIndex = 0;
+		for (int i = 0; i < this.row; i++) {
+			decimalIndex <<= 1;
+			decimalIndex += index[i];
+		}
+		return decimalIndex;
+	}
+
+	/*
+	 * main hashing function that calculate the index and handle different type of
+	 * key
+	 */
+	public <T> long hash(T key) {
+		long newKey;
+		if (key instanceof String) {
+			newKey = this.toLong((String) key);
+		} else if (key instanceof Character) {
+			newKey = this.toLong((Character) key);
+		} else
+			newKey = ((Number) key).longValue();
+		return this.getIndex(newKey);
+	}
+
+	/*
+	 * handle different types of keys
+	 */
+	private long toLong(char key) {
+		long newKey = (long) (key);
+		return newKey;
+	}
+
+	private long toLong(String key) {
+		StringBuilder binary = new StringBuilder();
+		for (int i = 0; i < key.length(); i++) {
+			int ascii = (int) key.charAt(i);
+			String binaryStr = Integer.toBinaryString(ascii);
+			binary.append(binaryStr);
+		}
+		long newKey = Long.parseLong(binary.toString(), 2);
+		return newKey;
+
 	}
 
 }
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		HashFunction hash = new HashFunction(0);
+		hash.hash("string");
+		
 	}
 
 }
